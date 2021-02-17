32e3c242d88d91ee33d14c490f302bf91583f18d
Manny <sancheze0450@gmail.com>
2021-02-17 00:17:36 -0500

    Initial commit

---
+++ Flixter/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Flixter
+//
+//  Created by Emmanuel Sanchez on 2/16/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var synopsisLabel: UILabel!
+    
+    @IBOutlet var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
