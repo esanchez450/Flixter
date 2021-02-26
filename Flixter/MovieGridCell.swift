e0f1560eea2e202b5939180fccebdc897d1d2ef6
Manny <sancheze0450@gmail.com>
2021-02-25 19:44:17 -0500

    Added collection view, navigation tab bar with items to project.

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Emmanuel Sanchez on 2/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
